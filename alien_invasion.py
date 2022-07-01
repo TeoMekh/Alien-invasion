33eb50ca3f8e9c32e2621b413b2d457fdca98d85
Fedor <mekhontseff@gmail.com>
2022-07-02 00:04:04 +0500

    Created spaceship

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,8 @@ import pygame
 
 from settings import Settings
 
+from ship import Ship
+
 
 class AlienInvasion:
     """ Класс для управления ресурсами и поведением игры. """
@@ -16,8 +18,7 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien invasion")
 
-        # Назначение цвета фона.
-        self.bg_color = (230, 230, 230)
+        self.ship = Ship(self)
 
     def run_game(self):
         """ Запуск основного цикла игры. """
@@ -29,6 +30,8 @@ class AlienInvasion:
 
             # При каждом проходе цикла перерисовывается экран.
             self.screen.fill(self.settings.bg_color)
+
+            self.ship.blitme()
 
             # Отображение последнего прорисованного экрана.
             pygame.display.flip()
