dfaa592e19f5726b17299be9326f0eb04aad71e9
Fedor <mekhontseff@gmail.com>
2022-07-02 11:40:47 +0500

    Code refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -23,18 +23,22 @@ class AlienInvasion:
     def run_game(self):
         """ Запуск основного цикла игры. """
         while True:
-            # Отслеживание событий клавиатуры и мыши.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.settings.bg_color)
+    def _check_events(self):
+        # Отслеживание событий клавиатуры и мыши.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            self.ship.blitme()
+    def _update_screen(self):
+        # При каждом проходе цикла перерисовывается экран.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
 
-            # Отображение последнего прорисованного экрана.
-            pygame.display.flip()
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
