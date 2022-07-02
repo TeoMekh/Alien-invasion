70f79885fb86a607c1753af760d9fabfcff6e865
Fedor <mekhontseff@gmail.com>
2022-07-02 12:26:08 +0500

    Added q-key for exit game

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,6 +15,10 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien invasion")
 
@@ -41,14 +45,16 @@ class AlienInvasion:
         """ Реагирует на нажатие клавиш."""
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
-        if event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         """ Реагирует на отпускание клавиш. """
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
-        if event.key == pygame.K_LEFT:
+        elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
 
