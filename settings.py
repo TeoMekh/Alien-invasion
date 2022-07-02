a1f22b3ce68e05dbda094b628acafd356bcb6269
Fedor <mekhontseff@gmail.com>
2022-07-02 12:09:28 +0500

    Added moves of the spacecraft

--- settings.py
+++ settings.py
@@ -8,3 +8,7 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (70, 70, 140)
+
+        # Настрояки корабля.
+        self.ship_speed = 1.5
+
