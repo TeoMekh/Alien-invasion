ba02eef262781e8136bb574b7399ef4995bff300
Fedor <mekhontseff@gmail.com>
2022-07-03 09:51:39 +0500

    Added bullets

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,8 @@ from settings import Settings
 
 from ship import Ship
 
+from bullet import Bullet
+
 
 class AlienInvasion:
     """ Класс для управления ресурсами и поведением игры. """
@@ -23,12 +25,22 @@ class AlienInvasion:
         pygame.display.set_caption("Alien invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
 
     def run_game(self):
         """ Запуск основного цикла игры. """
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+
+            # Удадение снарядов, вышедших за край экрана.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
+
             self._update_screen()
 
     def _check_events(self):
@@ -49,6 +61,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         """ Реагирует на отпускание клавиш. """
@@ -57,12 +71,19 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """  Создание нового снаряда и включение его в группу bullets. """
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
 
     def _update_screen(self):
         # При каждом проходе цикла перерисовывается экран.
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
-
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -12,3 +12,10 @@ class Settings:
         # Настрояки корабля.
         self.ship_speed = 1.5
 
+        # Параметры снаряда.
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (230, 250, 70)
+        self.bullets_allowed = 3
+
